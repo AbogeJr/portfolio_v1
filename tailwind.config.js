86d473fcfe49e68b1169bc18c37344ec17b7acca
AbogeJr <junioraboge@gmail.com>
2023-02-07 11:17:49 +0300

    fix: imported Montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+    },
     backgroundImage: {
       aboge: "url('/src/aboge.jpg')",
       texture: "url('/src/texture.jpg')",
