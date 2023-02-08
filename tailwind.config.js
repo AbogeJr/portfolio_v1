d99a893ed166110593d8a122b59f238a53c5da69
AbogeJr <junioraboge@gmail.com>
2023-02-08 14:54:40 +0300

    udate: add demo project && tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./*.html"],
   theme: {
     screens: {
       sm: "480px",
       md: "768px",
       lg: "976px",
       xl: "1440px",
-    },
-    fontFamily: {
-      montserrat: ["Montserrat", "sans-serif"],
     },
     backgroundImage: {
       aboge: "url('/src/aboge.jpg')",
